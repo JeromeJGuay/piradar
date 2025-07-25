dd53db21e1fbd60eab4a4f7812e86c837e109032
GUAYJ <jerome.guay@protonmail.com>
2025-07-25 08:49:26 -0400

    adding specific clone to branch

--- installer/install.sh
+++ installer/install.sh
@@ -135,7 +135,9 @@ cd
 
 mkdir -p "$HOME/program"
 cd "$HOME/program"
-git clone https://github.com/JeromeJGuay/piradar.git
+#git clone https://github.com/JeromeJGuay/piradar.git
+git clone -b main-v2.0.0 https://github.com/JeromeJGuay/piradar.git
+
 
 cd piradar
 
